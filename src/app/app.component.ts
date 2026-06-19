166e112cf4df10af581b2ad1be55fee086848bd0
Sanjana Awachat <sanjanaawachat1998@gmail.com>
2026-06-19 12:36:24 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'task3';
+ @ViewChild('userForm')userForm!:NgForm;
+onSubmit(form: NgForm) {
+    console.log(form.value);
+
+    alert('Registration Successful!');
+
+    form.reset();
+  }
+
 }
